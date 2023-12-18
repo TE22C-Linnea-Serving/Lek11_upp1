e8d611c0f558a468440c2c26816e70a2b1540044
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-12-18 09:00:59 +0100

    val 1

--- src/App.java
+++ src/App.java
@@ -13,13 +13,25 @@ public class App {
         if(val==1){
             Random slump = new Random();
             int slumptal1 = slump.nextInt(10);
-            System.out.println("Slumptal: "+slumptal1);
+            System.out.println("Slumptal1: "+slumptal1);
             Thread.sleep(100);
             int slumptal2 = slump.nextInt(10);
-            System.out.println("Slumptal: "+slumptal2);
-            
+            System.out.println("Slumptal2: "+slumptal2);
+
+            if(slumptal1>slumptal2){
+                int slumptal = slumptal1-slumptal2;
+                System.out.println("Skillnaden mellan slumptal1 och slumptal2 är: \""+slumptal+"\"");
+                System.out.println("Slumptal1 är större än slumptal2");
+            }else if(slumptal2>slumptal1){
+                int slumptal = slumptal2-slumptal1;
+                System.out.println("Skillnaden mellan slumptal1 och slumptal2 är: \""+slumptal+"\"");
+                System.out.println("Slumptal2 är större än slumptal1");
+            }else{
+                System.out.println("Slumptal1 och slumptal2 är lika stora");
+            }
             System.out.println("");
         }else if(val==2){
+            
 
         }
         }

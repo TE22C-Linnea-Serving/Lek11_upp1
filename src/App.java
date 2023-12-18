dbeb22d5e3d295baeb5a46fad787adf388fe03c0
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-12-18 08:47:46 +0100

    val 1

--- src/App.java
+++ src/App.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Skapa slumptalsgenerator
-        Random tärning = new Random();
-        // Skapa ett slumptal mellan 0-9
-        int slumptal = tärning.nextInt(10);
-        System.out.println("slumptal: " + slumptal);
-        // Pausa programmet
-        Thread.sleep(100);
-        // Skapa ett slumptal mellan 0-9
-        slumptal = tärning.nextInt(10);
-        System.out.println("slumptal: " + slumptal);
+        Scanner tangentbord = new Scanner(System.in);
+        int i=0;
+
+        while(i==0){
+        System.out.println("Meny: \n1. Slumptal \n2. Nedräkning \n3. Avsluta");
+        int val = tangentbord.nextInt();
+
+        if(val==1){
+            Random slump = new Random();
+            int slumptal1 = slump.nextInt(10);
+            System.out.println("Slumptal: "+slumptal1);
+            Thread.sleep(100);
+            int slumptal2 = slump.nextInt(10);
+            System.out.println("Slumptal: "+slumptal2);
+            
+            System.out.println("");
+        }else if(val==2){
+
+        }
+        }
     }
 }
